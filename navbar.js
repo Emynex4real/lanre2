007d7470e4158c079c775a4eea5661479032a04d
Michael <michaelbalogun34@gmail.com>
2024-12-12 01:11:48 +0100

    change

--- navbar.js
+++ navbar.js
@@ -3,12 +3,15 @@ const menu = document.querySelector('.menu');
 // const menu = document.querySelector('.menu');
 
 menu.addEventListener('click', (e)=>{
+    // sideBar.style.display = 'flex'
     sideBar.classList.add('active')
     e.stopPropagation();
 })
 window.addEventListener('click', ()=>{
     if(sideBar.classList.contains('active')){
         sideBar.classList.remove('active')
+        // sideBar.style.display = 'none'
+
     }
 })
 
